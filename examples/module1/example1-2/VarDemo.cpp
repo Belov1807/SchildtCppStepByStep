34328e73d3190d37987c6fc8c0ba7116eb48bc12
Belov1807 <belyi8814@gmail.com>
2022-02-05 18:06:54 +0300

    Добавлена программа демонстрирующая использование переменной.

---
+++ examples/module1/example1-2/VarDemo.cpp
@@ -0,0 +1,16 @@
+// Использование переменной.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int length;     // здесь объявляется переменная.
+    length = 7;     // здесь length присваивается значение 7.
+
+    cout << "Длина равна ";
+    cout << length; // здесь выводится 7;
+    cout << endl;
+    return 0;
+}
