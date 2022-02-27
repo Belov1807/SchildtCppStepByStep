2ca4fc350548b9e7440fa6751b76257e0918d068
Belov1807 <belyi8814@gmail.com>
2022-02-27 21:02:44 +0300

    Добавлена программа, демонстрирурующая использование функции abs().

---
+++ examples/module1/example1-10/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Программа, демонстрирурующая использование функции abs().
+ */
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    int result;
+
+    result = abs(-10);
+
+    cout << result <<endl;
+
+    return 0;
+}
