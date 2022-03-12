b8d2481f854552d70061a269fe6f33ab769cdc9c
Belov1807 <belyi8814@gmail.com>
2022-03-13 01:09:29 +0300

    Добавлена прог-ма, демонстрирующая некоторые Esc-последовательности.

---
+++ examples/module2/example2-5/main.cpp
@@ -0,0 +1,14 @@
+/*
+ * Демонстрация некоторых
+ * Esc-последовательностей.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "один\tдва\tтри\n";
+    cout << "123\b\b45";
+    cout << "\n";
+    return 0;
+}
