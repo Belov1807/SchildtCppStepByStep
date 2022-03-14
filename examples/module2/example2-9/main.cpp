aa65f63e765f7633b97fd5855fc6c3c5d3a34d0c
Belov1807 <belyi8814@gmail.com>
2022-03-14 06:50:23 +0300

    Добавлена программа, демонстрирующая приведение типа.

---
+++ examples/module2/example2-9/main.cpp
@@ -0,0 +1,13 @@
+/*
+ * Демонстрация приведения типа.
+*/
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for (i = 1; i <= 10; ++ i) cout << i << " / 2 равно: " << (float) i / 2 << endl;
+
+    return 0;
+}
