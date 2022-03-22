1cf10495db59b33528f2621ec08e1af9a514c8a0
Belov1807 <belyi8814@gmail.com>
2022-03-22 23:26:45 +0300

    Добавлена программа демонстрирующая цикл без приращения.

---
+++ examples/module3/example3-9/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Цикл без приращения.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+
+    for (x = 0; x != 123;)
+    {
+        cout << "Введите число: ";
+        cin >> x;
+    }
+    return 0;
+}
