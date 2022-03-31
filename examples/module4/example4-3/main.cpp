df4090ee1d566418d3afd57ee15320291dcba3f0
Belov1807 <belyi8814@gmail.com>
2022-03-31 21:58:09 +0300

    Добавлен пример 4-3 (заполнение двумерного массива числами 1-12).

---
+++ examples/module4/example4-3/main.cpp
@@ -0,0 +1,23 @@
+/*
+ * Двумерный массив заполняется
+ * числами от 1 до 12.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t, i, nums[3][4];
+
+    for (t = 0; t < 3; ++ t)
+    {
+        for (i = 0; i < 4; ++ i)
+        {
+            nums[t][i] = (t * 4) + i + 1;
+            cout << nums[t][i] << ' ';
+        }
+        cout << "\n";
+    }
+    return 0;
+}
