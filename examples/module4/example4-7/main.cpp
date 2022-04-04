f48e7a0073362ce076faa8a63e1bb7f0e7d7d742
Belov1807 <belyi8814@gmail.com>
2022-04-04 22:00:57 +0300

    Добавлен пример 4-7 (преобразование строчных символов в прописные):
    - Добавлена программа, демонстрирующая преобразование
    строчных символов строки в прописные (toupper()).

---
+++ examples/module4/example4-7/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Преобразование строчных символов
+ * строки в прописные (toupper()).
+ */
+
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    char str[80];
+    int i;
+
+    strcpy(str, "this is a test");
+
+    for (i = 0; str[i]; i ++) str[i] = toupper(str[i]);
+    cout << str << "\n";
+
+    return 0;
+}
