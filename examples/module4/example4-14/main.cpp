f0a7672c3d7f5238725fd1f74f838a7980d4ad8e
Belov1807 <belyi8814@gmail.com>
2022-04-05 22:20:15 +0300

    Добавлен пример 4-14(обр-ние регистра поср-вом индексации массива).

---
+++ examples/module4/example4-14/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * Обращение регистра посредством
+ * индексации массива.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    char str[80] = "This Is A Test";
+
+    cout << "Исходная строка: " << str << "\n";
+
+    for (i = 0; str[i]; i ++)
+    {
+        if (isupper(str[i])) str[i] = tolower(str[i]);
+        else if (islower(str[i])) str[i] = toupper(str[i]);
+    }
+
+    cout << "Преобразованная строка: " << str << "\n";
+
+    return 0;
+}
