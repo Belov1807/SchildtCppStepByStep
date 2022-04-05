70e8ccc4d7f4b78f75fc98b6dbb625058a562175
Belov1807 <belyi8814@gmail.com>
2022-04-05 22:04:05 +0300

    Добавлена п-ма, дем-щая присваивание значения посредством указателя.

---
+++ examples/module4/example4-12/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * Присваивание значения
+ * посредством указателя.
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int *p, num;
+
+    p = &num;
+    *p = 100;
+    cout << num << ' ';
+    (*p) ++;
+    cout << num << ' ';
+    (*p) --;
+    cout << num << "\n";
+
+    return 0;
+}
