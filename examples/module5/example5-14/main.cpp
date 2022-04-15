1a87552b5db08d40ae7d9c85580ada43d3a74f79
Belov1807 <belyi8814@gmail.com>
2022-04-15 22:58:09 +0300

    Добавлен пример 5-14(передача в функцию массива. 3-й способ):
    - Добавлена программа, демонстрирующая пример передачи
    в функцию массива в виде указателя.

---
+++ examples/module5/example5-14/main.cpp
@@ -0,0 +1,30 @@
+/*
+ * Передача в функцию
+ * массива в виде указателя.
+ */
+
+#include <iostream>
+using namespace std;
+
+void display(int *_num);
+
+int main()
+{
+    int t[10], i;
+
+    for (i = 0; i < 10; ++ i) t[i] = i;
+
+    /*
+     * Передача в функцию массива t.
+     */
+    display(t);
+
+    return 0;
+}
+
+void display(int _num[10])
+{
+    int i ;
+    for (i = 0; i < 10; i ++) cout << _num[i] << ' ';
+    cout << "\n";
+}
