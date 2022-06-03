e2818a38699374d45d7735d8375dc725403d70a6
Belov1807 <belyi8814@gmail.com>
2022-06-03 21:38:46 +0300

    Добавлен пример 7-5 (Пример использования enum).

---
+++ examples/module7/example7-5/main.cpp
@@ -0,0 +1,33 @@
+/*
+ * Демонстрация работы
+ * с перечислимым типом.
+ */
+#include <iostream>
+using namespace std;
+
+enum transport {car, truck, airplane, train, boat};
+
+char name[][27] =
+{
+    "car [автомобиль]",
+    "truck [грузовик]",
+    "airplane [самолет]",
+    "train [поезд]",
+    "boat [лодка]"
+};
+
+int main()
+{
+    transport how;
+
+    how = car;
+    cout << name[how] << '\n';
+
+    how = airplane;
+    cout << name[how] << '\n';
+
+    how = train;
+    cout << name[how] << '\n';
+
+    return 0;
+}
