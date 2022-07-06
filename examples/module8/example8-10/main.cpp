e9b76f91bfe62c6eeee82ac39fd29a9b89694de9
Belov1807 <belyi8814@gmail.com>
2022-07-06 22:32:55 +0300

    Добавлен пример 8-10 (Создание массива объектов).

---
+++ examples/module8/example8-10/main.cpp
@@ -0,0 +1,29 @@
+/*
+ * Создание массива объектов.
+ */
+#include <iostream>
+using namespace std;
+
+class MyClass
+{
+    int x;
+
+public:
+    void set_x(int _i) { x = _i; }
+    int get_x() { return x; }
+};
+
+int main()
+{
+    MyClass obs[4];
+    int i;
+
+    for (i = 0; i < 4; i ++)
+        obs[i].set_x(i);
+
+    for(i = 0; i < 4; i ++)
+        cout << "obs[" << i << "].get_x(): "
+             << obs[i].get_x() << '\n';
+
+    return 0;
+}
